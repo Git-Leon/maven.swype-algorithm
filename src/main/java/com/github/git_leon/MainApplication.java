2c186ab8395fad2135ffa32d4652dfeaeb180008
leon hunter <xleonhunter@gmail.com>
2020-04-06 16:40:10 -0400

    eliminated superfluous classes

--- src/main/java/com/github/git_leon/MainApplication.java
+++
@@ -1,9 +0,0 @@
-package com.github.git_leon;
-
-public class MainApplication {
-    public static void main(String[] args) {
-        MyObject myObject = new MyObject();
-        myObject.setup();
-        myObject.run();
-    }
-}
